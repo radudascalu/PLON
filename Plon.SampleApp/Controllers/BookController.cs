fc2aeed0bc136c36830e49c30c0bfd7e6d40ba4c
radudascalu <dascalu.raduciprian@yahoo.com>
2018-03-06 23:31:38 +0100

    Added sample API method.

--- Plon.SampleApp/Controllers/BookController.cs
+++ Plon.SampleApp/Controllers/BookController.cs
@@ -1,8 +1,15 @@
-﻿using System.Web.Mvc;
+﻿using Plon.SampleApp.Models;
+using PLON.Core;
+using System.Web.Http;
 
 namespace Plon.SampleApp.Controllers
 {
-    public class BookController : Controller
+    public class BookController : ApiController
     {
+        [PlonModel(typeof(Book))]
+        public IHttpActionResult Get(int id)
+        {
+            return Ok(new Book { Id = id, Name = "Abc" });
+        }
     }
 }
---
+++ Plon.SampleApp/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Plon.SampleApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
